25604533a2f40877738c881acfc4477d7efbb5a2
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-02-10 16:45:53 -0500

    Added proxy

---
+++ proxy.js
@@ -0,0 +1,11 @@
+var proxy = require('redbird')({
+    port: 80,
+    secure: true,
+    ssl: {
+        port: 443,
+        key: "./ssl/private.key",
+        cert: "./ssl/certificate.cer",
+    }
+});
+
+proxy.register("domain.com", "http://localhost:8000", {ssl: true});
