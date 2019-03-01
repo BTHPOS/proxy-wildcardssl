ae7adb6a6aed4cf6109af63084c278ef74e15957
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-02-28 19:29:51 -0500

    Updated proxy to new insight servers

--- proxy.js
+++ proxy.js
@@ -9,9 +9,9 @@ var proxy = require('redbird')({
 });
 
 
-proxy.register("insight.bithereum.network", "http://3.90.43.240:8000", {ssl: false});
-proxy.register("explorer.bithereum.network", "http://3.90.43.240:8000", {ssl: false});
-proxy.register("insight2.bithereum.network", "http://3.90.43.240:8000", {ssl: false});
+proxy.register("insight.bithereum.network", "http://18.206.164.133:3000", {ssl: false});
+proxy.register("explorer.bithereum.network", "http://18.206.164.133:3000", {ssl: false});
+proxy.register("insight2.bithereum.network", "http://96.56.229.210:3000", {ssl: false});
 
 
 
